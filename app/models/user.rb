0d395a76ed29ff2c975881b8a5b41209249ca321
Osame-Yu <93514585+Osame-Yu@users.noreply.github.com>
2021-11-26 12:34:54 +0900

    userモデルのバリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Input full-width characters'}
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Input full-width characters'}
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'Input full-width katakana characters'}
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'Input full-width katakana characters'}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'must be full-width characters'}
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'must be full-width characters'}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'must be full-width katakana characters'}
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'must be full-width katakana characters'}
   validates :birthday, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?[\d])[a-zA-Z\d]+\z/, message: 'must include both numbers and letters'}
 end
