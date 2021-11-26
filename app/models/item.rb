ec7960c6b60734bfec8918d7028f2be4b08d21f3
Osame-Yu <93514585+Osame-Yu@users.noreply.github.com>
2021-11-26 17:23:04 +0900

    各モデルファイルにアソシエーションとバリデーションを追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,18 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  belongs_to :category
+  belongs_to :state
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_day
 
   validates :name, presence: true
   validates :desdription, presence: true
   validates :price, presence: true
-  validates :category_id, presence: true
-  validates :state_id, presence: true
-  validates :shipping_cost_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :shipping_id, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :state_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
