76385c57cb6b05b6808c2501f531a9f74f246877
Osame-Yu <93514585+Osame-Yu@users.noreply.github.com>
2021-11-26 16:57:02 +0900

    Itemモデルのバリデーション設定とアソシエーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  validates :name, presence: true
+  validates :desdription, presence: true
+  validates :price, presence: true
+  validates :category_id, presence: true
+  validates :state_id, presence: true
+  validates :shipping_cost_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_id, presence: true
 end
