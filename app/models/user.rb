e805d62b8a24380e0ee7fae28752e2da62e05aaa
Osame-Yu <93514585+Osame-Yu@users.noreply.github.com>
2021-11-26 14:15:06 +0900

    バリデーションの正規表現を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'must be full-width characters'}
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'must be full-width characters'}
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'must be full-width katakana characters'}
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'must be full-width katakana characters'}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'must be full-width katakana characters'}
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'must be full-width katakana characters'}
   validates :birthday, presence: true
   validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?[\d])[a-zA-Z\d]+\z/, message: 'must include both numbers and letters'}
 end
