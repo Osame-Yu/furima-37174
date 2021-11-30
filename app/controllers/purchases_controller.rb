ddc84c12b1e6496e0efaa95705fb3bf34f8dddf9
Osame-Yu <93514585+Osame-Yu@users.noreply.github.com>
2021-11-30 12:40:59 +0900

    フォームオブジェクトのインスタンス生成

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,15 @@
 class PurchasesController < ApplicationController
+
+  def index
+    @purchase_delivery = PurchaseDelivery.new
+  end
+
+  def create
+    @purchase_delivery = PurchaseDelivery.new(purchase_params)
+  end
+
+  private
+  def purchase_params
+    require(:purchase_delivery).permit(:post_num, :prefecture, :city, :address, :building, :phone).
+                                merge(user_id: current_user.id, item_id: parame[:item_id])
 end
