3fef41af3192a7e14d5e20a0422c6f1ccb31c780
Osame-Yu <93514585+Osame-Yu@users.noreply.github.com>
2021-12-01 23:36:39 +0900

    クレジットカードによる商品購入機能の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,15 +1,35 @@
 class PurchasesController < ApplicationController
 
   def index
+    @item = Item.find(params[:item_id])
     @purchase_delivery = PurchaseDelivery.new
   end
 
   def create
     @purchase_delivery = PurchaseDelivery.new(purchase_params)
+    if @purchase_delivery.valid?
+      pay_item
+      @purchase_delivery.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
   end
 
   private
   def purchase_params
-    require(:purchase_delivery).permit(:post_num, :prefecture, :city, :address, :building, :phone).
-                                merge(user_id: current_user.id, item_id: parame[:item_id])
+    params.require(:purchase_delivery).permit(:post_num, :prefecture_id, :city, :address, :building, :phone)
+      .merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    item = Item.find(params[:item_id])
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
+  end
 end
--- app/models/purchase_delivery.rb
+++ app/models/purchase_delivery.rb
@@ -1,22 +1,21 @@
 class PurchaseDelivery
 
-  include ActiveModel:: Modele
-  attr_accesser :item, :user_id
-                :post_num, :prefecture_id, :city, :address, :building, :phone, :purchase_id,
+  include ActiveModel:: Model
+  attr_accessor :item_id, :user_id, :post_num, :prefecture_id, :city, :address, :building, :phone, :token
 
   with_options presence: true do
-    validates :item
+    validates :item_id
     validates :user_id
-    validates :post_num, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'must include both numbers and hyphen(-)' }
+    validates :post_num, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "must include both numbers and hyphen(-)" }
     validates :city
     validates :address
-    validates :phone, numericality: { only_integer: true, message: 'must be numbers' }
-    validates :purchase_id
+    validates :phone, numericality: { only_integer: true, message: "must be numbers" }
+    validates :token
   end
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
-    purchase = Purchase.create(item: item, user_id: user_id)
-    Delivery.create(post_num: post_num, city: city, address: address, phone: phone, purchase_id: purchase_id)
+    purchase = Purchase.create(item_id: item_id, user_id: user_id)
+    Delivery.create(post_num: post_num, prefecture_id: prefecture_id, city: city, address: address, phone: phone, purchase_id: purchase.id)
   end
 end
