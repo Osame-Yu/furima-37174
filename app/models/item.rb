0bd16185df54ef0851a05168a3d435352ff4579d
Osame-Yu <93514585+Osame-Yu@users.noreply.github.com>
2021-12-03 16:42:00 +0900

    S3の導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
   belongs_to :shipping_cost
   belongs_to :prefecture
   belongs_to :shipping_day
-  belongs_to :purchase
+  has_one :purchase
 
   with_options presence: true do
     validates :image
